35fff641a18e0b047fd4d61f6b4a2d56cb199775
Deepu kashyap <109035551+codingFrog7@users.noreply.github.com>
2025-06-10 13:58:46 +0530

    Update frog.jsx

--- src/components/frog.jsx
+++ src/components/frog.jsx
@@ -5,7 +5,7 @@ const Frog = () => {
     < div className="w-full h-screen absolute top-0  flex justify-center items-center">
       <div className=" h-1/2 w-screen flex justify-center items-center  ">
         <Spline scene="https://prod.spline.design/Hn1UzE9WH0haIpB5/scene.splinecode" />
-        <div className="h-16 w-36 bg-black absolute right-8 bottom-[27vh] "></div>
+        <div className="h-16 w-36 bg-black absolute right-4 bottom-[27vh] "></div>
       </div>
     </div>
   )
